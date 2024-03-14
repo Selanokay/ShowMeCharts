d3bdf3b535161319c882a98175d9b3978a592d42
Selanokay <83822255+Selanokay@users.noreply.github.com>
2024-03-14 14:41:50 -0500

    Update server.js
    Updated server.js, replaced listen with connect to db, catching for errors!

--- Capstone/Backend/backend/server.js
+++ Capstone/Backend/backend/server.js
@@ -17,7 +17,14 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/songs', songRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port', process.env.PORT)
-})
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('Listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
