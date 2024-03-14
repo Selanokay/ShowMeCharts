42713843c9686073535893f93b190e0da32be9b8
Jonathan D Marquez <jdmtrc@umsystem.edu>
2024-03-14 13:15:50 -0500

    Unzipped the backend folder and added it to repo

---
+++ Capstone/Backend/backend/routes/songs.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const router = express.Router()
+
+//GET all songs
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all songs'})
+})
+
+//GET a single song
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET single song'})
+})
+
+//POST a new song
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new song'})
+})
+
+//DELETE a song
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE a song'})
+})
+
+//UPDATE a song
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE a song'})
+})
+
+module.exports = router
