ce5a31d717c7c4a2fca048d944fe745ac2ae74d6
Selanokay <grrvvt@umsystem.edu>
2024-03-20 09:46:21 -0500

    Archived old work, moved pictures into Images fldr

---
+++ Capstone/Archived/signup.js
@@ -0,0 +1,7 @@
+let uEmail = document.getElementById('uemail');
+console.log(uEmail)
+console.log("working")
+function getSignupinfo() {
+    document.getElementById("signup1").onsubmit();
+}
+console.log(getSignupinfo)
--- Capstone/Frontend/signup.js
+++
@@ -1,7 +0,0 @@
-let uEmail = document.getElementById('uemail');
-console.log(uEmail)
-console.log("working")
-function getSignupinfo() {
-    document.getElementById("signup1").onsubmit();
-}
-console.log(getSignupinfo)
