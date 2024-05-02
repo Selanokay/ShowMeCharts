d7399fb5de6afb8081457202c1f1b8e3a3266ea8
peternielsen15 <jpnqdf@umsystem.edu>
2024-05-02 15:10:32 -0500

    profile page

--- Frontend/src/pages/profile.js
+++ Frontend/src/pages/profile.js
@@ -1,43 +1,109 @@
 import React from 'react';
-import logoImage from '../images/logo.png';
+import examplepfp from '../images/examplepfp.jpg';
+import gradThumbnail from '../images/grad.jpg';
+import currThumbnail from '../images/currents.jpg';
+import crueThumbnail from '../images/cru.jpg';
+import astroThumbnail from '../images/astro.png';
+import d4cThumbnail from '../images/d4c.jpg';
+import doomThumbnail from '../images/mfdoom.jpg';
+import voidzThumbnail from '../images/voids.png';
 
-export default function Profile() { 
+export default function Profile() {
     return (
-       //Profile Info
         <div className="header">
-            <br></br>
-            <br></br>
-            <br></br>
             <div className="profile-header">
-                <img src={logoImage} alt="Profile Logo" width="150" height="150"/>
-                    <div className="profile-info">
-                        <h1>Name: </h1>
-                        <h1>School: </h1>
-                    </div>
+                <div className="profile-image-container">
+                    <img src={examplepfp} alt="Profile Logo" width="150" height="150" className="profile-image"/>
+                </div>
+                <div className="profile-info">
+                    <h1>Name: Peter Nielsen</h1>
+                    <h1>School: University of Missouri-Columbia</h1>
+                </div>
             </div>
-            <br></br>
             <div>
-                <h2>Your Top Songs</h2>
-                <ul>
+                <h2 className="section-header">Your Top Songs</h2>
+                <ul className="top-list">
                     <li>
                         <table>
                             <tr>
-                                <td><img src={logoImage} alt="Top Song" /></td>
-                                <td>Artist Name</td>
-                                <td>Song Title</td>
+                                <td className="thumbnail-cell"><img src={gradThumbnail} alt="Top Song"/></td>
+                                <td>Flashing Lights - Kanye West</td>
+                            </tr>
+                        </table>
+                    </li>
+                    <li>
+                        <table>
+                            <tr>
+                                <td className="thumbnail-cell"><img src={currThumbnail} alt="Top Song"/></td>
+                                <td>New Person, Same Old Mistakes - Tame Impala</td>
+                            </tr>
+                        </table>
+                    </li>
+                    <li>
+                        <table>
+                            <tr>
+                                <td className="thumbnail-cell"><img src={crueThumbnail} alt="Top Song"/></td>
+                                <td>Kickstart My Heart - Mötley Crüe</td>
+                            </tr>
+                        </table>
+                    </li>
+                    <li>
+                        <table>
+                            <tr>
+                                <td className="thumbnail-cell"><img src={astroThumbnail} alt="Top Song"/></td>
+                                <td>Coffee Bean - Travis Scott</td>
+                            </tr>
+                        </table>
+                    </li>
+                    <li>
+                        <table>
+                            <tr>
+                                <td className="thumbnail-cell"><img src={d4cThumbnail} alt="Top Song"/></td>
+                                <td>Dirty Deeds Done Dirt Cheap - AC/DC</td>
                             </tr>
                         </table>
                     </li>
                 </ul>
-                <br></br>
-                <h2>Your Top Artists</h2>
-                <ul>
+                <br/>
+                <h2 className="section-header">Your Top Artists</h2>
+                <ul className="top-list">
                     <li>
                         <table>
                             <tr>
-                                <td><img src={logoImage} alt="Top Artist" /></td>
-                                <td>Artist Name</td>
-                                <td>Song Title</td>
+                                <td className="thumbnail-cell"><img src={gradThumbnail} alt="Top Song"/></td>
+                                <td>Kanye West</td>
+                            </tr>
+                        </table>
+                    </li>
+                    <li>
+                        <table>
+                            <tr>
+                                <td className="thumbnail-cell"><img src={astroThumbnail} alt="Top Song"/></td>
+                                <td>Travis Scott</td>
+                            </tr>
+                        </table>
+                    </li>
+                    <li>
+                        <table>
+                            <tr>
+                                <td className="thumbnail-cell"><img src={currThumbnail} alt="Top Song"/></td>
+                                <td>Tame Impala</td>
+                            </tr>
+                        </table>
+                    </li>
+                    <li>
+                        <table>
+                            <tr>
+                                <td className="thumbnail-cell"><img src={doomThumbnail} alt="Top Song"/></td>
+                                <td>MF DOOM</td>
+                            </tr>
+                        </table>
+                    </li>
+                    <li>
+                        <table>
+                            <tr>
+                                <td className="thumbnail-cell"><img src={voidzThumbnail} alt="Top Song"/></td>
+                                <td>The Voidz</td>
                             </tr>
                         </table>
                     </li>
