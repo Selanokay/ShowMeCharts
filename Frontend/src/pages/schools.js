a86a07218100dae99ff857ebab01958562a36520
polajas <89672655+polajas@users.noreply.github.com>
2024-05-04 01:47:32 +1000

    Update schools.js

--- Frontend/src/pages/schools.js
+++ Frontend/src/pages/schools.js
@@ -7,3 +7,59 @@ export default function Schools() {
         </div>
     )
 }
+
+
+
+/*import React from 'react';
+import school from '../images/schools.png';
+import Mizzou from '../images/mulogo.png';
+import Texas from '../images/utexaslogo.png';
+import Florida from '../images/uflorida.png';
+import UCLA from '../images/ucla.png';
+
+//Schools
+export default function Schools() {
+    return (
+        <div className="header">
+                <h1>ShowMeCharts</h1>
+                <br></br>
+            <br></br>
+            <div className="school-header">
+                <img src={school} alt="School Logo" width="100" height="100"/>
+                    <div className="school-info">
+                        <h1>Schools: </h1>
+                        <h1> </h1>
+                    </div>
+            </div>
+            <div>
+            <h2>Univeristy of Missouri Top 10 Songs</h2>
+            <td><img src={school} alt="Mizzou" /></td>
+                <ul>
+                    <li>
+                        <table>
+                            <tr>
+                                <h2>Univeristy of Texas: Top 10 Songs</h2>
+                                <td><img src={Texas} alt="Texas" /></td>
+                            </tr>
+                        </table>
+                    </li>
+                </ul>
+                <br></br>
+                    <h2>Univeristy of Florida: Top 10 Songs</h2>
+                    <td><img src={school} alt="Florida" /></td>
+                <ul>
+                    <li>
+                        <table>
+                            <tr>
+                                <h2>UCLA: Top 10 Songs</h2>
+                                <td><img src={school} alt="ucla" /></td>
+                            </tr>
+                        </table>
+                    </li>
+                </ul>
+            </div>
+    
+        </div>
+        )
+ }
+*/
